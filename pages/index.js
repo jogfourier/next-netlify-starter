e1025d4d087d617a6e403324d51e7d073fafa016
Guillermo Jimenez <38506930+jogfourier@users.noreply.github.com>
2022-02-21 14:16:33 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,51 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import * as React from 'react'
+import {registerSection} from '../components/register-section'
+import pdfMake from 'pdfmake/build/pdfmake'
+
+// I uploaded my font to AWS S3 and set up CORS for it.
+const fonts = {
+  yourFontName: {
+    normal: 'https://okt.s3.us-west-2.amazonaws.com/ipaexg.ttf',
+    bold: 'https://okt.s3.us-west-2.amazonaws.com/ipaexg.ttf',
+    italics: 'https://okt.s3.us-west-2.amazonaws.com/ipaexg.ttf',
+    bolditalics: 'https://okt.s3.us-west-2.amazonaws.com/ipaexg.ttf',
+  },
+}
+const docDefinition = {
+  pageMargins: [20, 97, 20, 60] as [number, number, number, number],
+  pageSize: {
+    width: 595.28,
+    height: 879,
+  },
+  content: [{...registerSection()}],
+  styles: {},
+  images: {
+    snow: 'https://okt.s3.us-west-2.amazonaws.com/logo.png',
+  },
+  defaultStyle: {
+    fontSize: 10,
+    font: 'yourFontName',
+  },
+}
+
+function ClientSidePdf() {
+  function openPdf() {
+    // @ts-ignore
+    pdfMake.createPdf(docDefinition, null, fonts).open()
+  }
+
+  return (
+    <div>
+      ClientSidePdf
+      <button onClick={openPdf}>Open</button>
+    </div>
+  )
+}
+
+export default ClientSidePdf
 
 export default function Home() {
   return (
