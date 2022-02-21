ace6a18cd57a8929a6f67b1384a11f6a068801f9
Guillermo Jimenez <38506930+jogfourier@users.noreply.github.com>
2022-02-21 13:59:41 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,34 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+          <form name="contact" method="POST" data-netlify="true">
+        <p>
+          <label>Nombre (nombre completo): <input type="text" name="nombre" /></label>   
+        </p>
+        <p>
+          <label>Fecha de Nacimiento (dd/mm/aaaa): <input type="text" name="fecha" /></label>
+        </p>
+        <p>
+          <label>Edad: <input type="text" name="fecha" /></label>
+        </p>
+        <p>
+          <label>Pasaporte: <input type="text" name="pasaporte" /></label>
+        </p>
+
+
+        <p>
+          <label>Your Role: <select name="role[]" multiple>
+            <option value="leader">Leader</option>
+            <option value="follower">Follower</option>
+          </select></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <button type="descargar">Send</button>
+        </p>
+      </form>
       </main>
 
       <Footer />
