cc566279a41f4895a9ea464ad68e84eb6854c8c7
Guillermo Jimenez <jog.fourie@gmail.comr>
2022-11-12 14:32:57 -0600

    Cambio 1

--- pages/index.js
+++ pages/index.js
@@ -56,7 +56,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to my app, VSCode!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
