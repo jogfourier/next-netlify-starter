359d5c14485ccb778f7588be473d91f4665b9f78
Guillermo Jimenez <jog.fourie@gmail.comr>
2022-11-12 14:48:42 -0600

    Cambio 3

--- pages/index.js
+++ pages/index.js
@@ -83,34 +83,3 @@ export default function Home() {
     </div>
   )
 }
-
-
-document.getElementById("app").innerHTML = `
-<h1>File Upload & FormData Example</h1>
-<div>
-<input type="file" id="fileInput" />
-</div>
-`;
-
-const fileInput = document.querySelector("#fileInput");
-
-const uploadFile = file => {
-  console.log("Uploading file...");
-  const API_ENDPOINT = "https://file.io";
-  const request = new XMLHttpRequest();
-  const formData = new FormData();
-
-  request.open("POST", API_ENDPOINT, true);
-  request.onreadystatechange = () => {
-    if (request.readyState === 4 && request.status === 200) {
-      console.log(request.responseText);
-    }
-  };
-  formData.append("file", file);
-  request.send(formData);
-};
-
-fileInput.addEventListener("change", event => {
-  const files = event.target.files;
-  uploadFile(files[0]);
-});
