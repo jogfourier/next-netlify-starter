32058f57f352cfe59b87ee7de4587fc33b9539c6
Guillermo Jimenez <jog.fourie@gmail.comr>
2022-11-12 16:10:22 -0600

    7

--- pages/index.js
+++ pages/index.js
@@ -84,19 +84,8 @@ export default function Home() {
         <ul id="output"></ul>
         <script>
           const output = document.getElementById('output');
-          if (window.FileList && window.File) {
-            document.getElementById('file-selector').addEventListener('change', event => {
-              output.innerHTML = '';
-              for (const file of event.target.files) {
-                const li = document.createElement('li');
-                const name = file.name ? file.name : 'NOT SUPPORTED';
-                const type = file.type ? file.type : 'NOT SUPPORTED';
-                const size = file.size ? file.size : 'NOT SUPPORTED';
-                li.textContent = `name: ${name}, type: ${type}, size: ${size}`;
-                output.appendChild(li);
-              }
-            })
-          }
+
+         
         </script>
 
 
